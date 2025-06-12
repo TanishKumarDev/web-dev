3106190ed4eab80bc2e4a9639c54edfc4f69669d
Tanish Kumar <tanish.kumar.contact@gmail.com>
2025-06-13 05:17:42 +0530

    Re -  structure projects

--- Projects/truefeedback/src/Schema/acceptMessageSchema.ts
+++
@@ -1,8 +0,0 @@
-// src/Schema/acceptMessageSchema.ts
-
-import { z } from "zod";
-
-// Schema to validate user's message acceptance preference (true/false)
-export const acceptMessageSchema = z.object({
-  acceptMessage: z.boolean(),
-});

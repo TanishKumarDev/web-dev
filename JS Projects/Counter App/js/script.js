0eead12eb31817b53d814636fd82cce7720acc22
Tanish <tanish.kumar.contact@gmail.com>
2025-07-06 10:37:09 +0530

    refactor: Counter App

--- JS Projects/Counter App/js/script.js
+++ JS Projects/Counter App/js/script.js
@@ -8,36 +8,28 @@ const message = document.getElementById("message");
 let counter = Number(localStorage.getItem("counterValue")) || 0;
 
 function updateCounter() {
-    count.textContent = counter;
+  count.textContent = counter;
+  localStorage.setItem("counterValue", counter);
 
-    localStorage.setItem("counterValue", counter);
-    
-    if (counter > 18) {
-        message.textContent = "You are an adult!";
-    } else {
-        message.textContent = ""; // Clear if not
-    }
-
-    //  if (counter > 0) {
-    //     counterDisplay.style.color = "green";
-    // } else if (counter < 0) {
-    //     counterDisplay.style.color = "red";
-    // } else {
-    //     counterDisplay.style.color = "white";
-    // }
+  if (counter > 18) {
+    message.textContent = "You are an adult!";
+  } else {
+    message.textContent = ""; // Clear if not
+  }
 }
 incrementBtn.addEventListener("click", () => {
-    counter++;
-    updateCounter();
-})
+  counter++;
+  updateCounter();
+});
 decrementBtn.addEventListener("click", () => {
-    counter--;
-    updateCounter();
-})
+  counter--;
+  updateCounter();
+});
 resetBtn.addEventListener("click", () => {
-    counter = 0;
-    localStorage.removeItem("counterValue");
-    updateCounter();
-})
+  counter = 0;
+  localStorage.removeItem("counterValue");
+  updateCounter();
+});
+
 // Load saved value on page refresh
 updateCounter();
